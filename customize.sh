ce8b5db4f988272f91cbf88e3df3459a6f28fbdc
hwlon <hwlon@163.com>
2021-04-06 21:28:02 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
